a1e3b8e674ef55f3afe465c334ed0deec442994a
saulocn <saulocn@gmail.com>
2018-11-24 12:04:07 -0200

    - Início do Projeto;

---
+++ m01/src/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+import React from 'react'
+
+const App =  React.createClass({
+    render: function () {
+        return <div>Aplicação 1</div>
+    }
+})
+
+
+
+
+export default App
---
+++ m01/src/index.js
@@ -0,0 +1,25 @@
+'use strict'
+// deve ser sempre as primeiras linhas do arquivo
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './app'
+
+
+const renderApp = (NextApp) =>{
+    render(
+        <AppContainer>
+            <NextApp />
+        </AppContainer>,
+        document.querySelector('[data-js="app"]')
+    )
+}
+
+renderApp(App)
+
+if(module.hot){
+    module.hot.accept('./app', ()=>{
+        const NextApp = require('./app').default
+        renderApp(NextApp)
+    })
+}
