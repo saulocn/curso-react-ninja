50d1eb84eb97e413bdd3bbc4fd8dba23b355c94b
saulocn <saulocn@gmail.com>
2018-11-24 10:54:34 -0200

    Importando somente a função render do ReactDOM

--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -2,10 +2,10 @@
 // deve ser sempre as primeiras linhas do arquivo
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import Title from './app'
 
-ReactDOM.render(
+render(
     React.createElement(Title),
     document.querySelector('[data-js="app"]')
 )
