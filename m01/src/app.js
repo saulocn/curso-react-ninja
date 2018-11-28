348a7ce4e398a3fd5c15e243ac6942de64c41f7a
saulocn <saulocn@gmail.com>
2018-11-27 22:13:55 -0200

    - Remoção dos Comentários;

--- m01/src/app.js
+++ m01/src/app.js
@@ -1,9 +1,9 @@
 'use strict'
 
-import React from 'react'
+import React, { Component } from 'react'
 import Title from './title'
 
-class App  extends React.Component {
+class App  extends Component {
     render () {
         return (
             <div className='container'>
@@ -12,20 +12,5 @@ class App  extends React.Component {
         )
     }
 }
-
-
-/*
-const App =  React.createClass({
-    render: function () {
-        return <div className='container'>
-            <Title name='Saulo' lastName={{first:'Carvalho', last:'Neto'}}  />
-            <label htmlFor='input' data-label='Label'>Input</label>
-            <input type='text' id='input' aria-hidden='{true}'/>
-        </div>
-    }
-})
-*/
-
-
 
 export default App
--- m01/src/title.js
+++ m01/src/title.js
@@ -14,20 +14,4 @@ Title.defaultProps = {
     }
 }
 
-/*
-const Title = React.createClass({
-    getDefaultProps: function(){
-        return {
-            name : 'Desconhecido',
-            lastName : {
-                first:'Sem',
-                last:'Sobrenome'
-            }
-        }
-    },
-    render: function(){
-        return <h1>Olá {this.props.name} {this.props.lastName.first} {this.props.lastName.last}!</h1>
-    }
-})*/
-
 export default Title
