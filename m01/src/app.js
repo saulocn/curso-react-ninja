ed3b901a3aaf21050a09064a592040d180b5b28c
saulocn <saulocn@gmail.com>
2018-11-27 22:27:13 -0200

    - Loop de Components;

--- m01/src/app.js
+++ m01/src/app.js
@@ -2,12 +2,16 @@
 
 import React, { Component } from 'react'
 import Title from './title'
+import Square from './square'
 
 class App  extends Component {
     render () {
         return (
             <div className='container'>
                 <Title name='Saulo' lastName={{first:'Carvalho', last:'Neto'}}  />
+                {['blue', 'red', 'green'].map((square) => (
+                    <Square key={} color={square} />
+                ))}
             </div>
         )
     }
