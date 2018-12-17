c7d292b81a28a0be881d7f5d2e81dba6b91e68bf
saulocn <saulocn@gmail.com>
2018-12-17 18:44:49 -0200

    Utilização de composição e funções nos componentes

--- m01/src/app.js
+++ m01/src/app.js
@@ -1,16 +1,15 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button'
+import LikeButton from './like-button'
+import SearchButton from './search-button'
 
-class App  extends Component {
-    render () {
+class App extends Component {
+    render() {
         return (
             <div className='container' >
-                <Button>
-                    <span>Texto</span>
-                    Outro texto
-                </Button>
+                <LikeButton />
+                <SearchButton />
             </div>
         )
     }
