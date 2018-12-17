6a4751762d1691d64fae1a3cb983d75ef7173361
saulocn <saulocn@gmail.com>
2018-12-17 21:53:55 -0200

    Fluxo de dados do React

--- m01/src/app.js
+++ m01/src/app.js
@@ -1,21 +1,25 @@
 'use strict'
 
 import React, { Component } from 'react'
+import Button from './button'
+import Square from './square';
 
 class App extends Component {
-    constructor() {
+    constructor(){
         super()
         this.state = {
-            text: 'Saulo'
+            color:'green'
         }
     }
     render() {
         return (
-            <div className='container' onClick={() =>
-                this.setState({
-                    text: 'Mudou o estado!'
-                })} >
-                {this.state.text}
+            <div>
+                <Square color={this.state.color} />
+                {['red', 'green', 'blue'].map((color) => (
+                    <Button key={color} handleClick={()=> this.setState({color})}>
+                        {color}
+                    </Button>
+                ))}
             </div>
         )
     }
