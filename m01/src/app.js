05e061964e480afb9c643a7e1d4fb0c3ccf6b627
saulocn <saulocn@gmail.com>
2018-12-20 18:31:21 -0200

    Forms checkbox e radio

--- m01/src/app.js
+++ m01/src/app.js
@@ -1,20 +1,58 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button';
 
 class App extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            value:'Valor inicial'
+            value: 'Valor inicial',
+            checked: false
         }
     }
     render() {
         return (
             <div>
                 <form>
-                    <input type='text' defaultValue='Valor inicial' />
+                    <input type='text'
+                        value={this.state.value}
+                        onChange={(e) => {
+                            this.setState({
+                                value: e.target.value
+                            })
+                        }} />
+                    <br />
+                    <br />
+
+                    <label>
+                        <input
+                            type="checkbox"
+                            value='meu-checkbox'
+                            checked={this.state.checked}
+                            onChange={
+                                (e) => {
+                                    this.setState({ checked: !this.state.checked })
+                                }
+                            }
+                        />
+                        Checkbox
+                    </label>
+                    <br />
+                    <br />
+                    <input 
+                    type='radio'
+                    name='rd'
+                    defaultChecked
+                    value='1'/> 
+                    Radio 1
+                    <input 
+                    type='radio'
+                    name='rd'
+                    value='2'/> 
+                    Radio 2
+                    <br />
+                    <br />
+
                 </form>
             </div>
         )
