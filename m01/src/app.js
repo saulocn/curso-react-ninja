e2065cb7437d12cee65a759098672efd5272695f
saulocn <saulocn@gmail.com>
2018-12-20 18:06:04 -0200

    Início de Proptypes

--- m01/src/app.js
+++ m01/src/app.js
@@ -1,32 +1,15 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Timer from './timer'
+import Button from './button';
 
 class App extends Component {
-    constructor(){
-        super()
-        console.log('app: constructor')
-        this.state = {
-            showTimer : true,
-            time: 0
-        }
-    }
-    componentWillMount(){
-        console.log('app: componentWillMount')
-    }
-    componentDidMount(){
-        console.log('app: componentDidMount')
-    }
-    componentWillUnmount
     render() {
-        console.log('app: render')
         return (
             <div>
-                <Timer time={this.state.time} /> 
-                <button onClick={()=>{
-                    this.setState({time:this.state.time+10})
-                }}>Change props</button>
+                <Button handleClick={()=>console.log('clicou')}>
+                    Clique em mim
+                </Button>
             </div>
         )
     }
