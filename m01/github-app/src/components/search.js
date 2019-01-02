df4048f7c1f77df4120168d1e07b614998d00a2d
saulocn <saulocn@gmail.com>
2019-01-02 17:49:37 -0200

    Consultando os dados da API

--- m01/github-app/src/components/search.js
+++ m01/github-app/src/components/search.js
@@ -1,11 +1,25 @@
 'use strict'
 import React from 'react'
+import ajax from '@fdaciuk/ajax'
 
 const Search = () => (
     <div className='search'>
         <input
             type='search'
             placeholder='Digite o nome do usuário no GitHub'
+            onKeyUp={e=>{
+                const keyCode = e.which || e.keyCode
+                const ENTER = 13
+                if(keyCode=== ENTER){
+                    ajax().get(`https://api.github.com/users/${e.target.value}`)
+                    .then(result=>{
+                        console.log(result)
+                    })
+                }
+            }}
+            onChange={e=>{
+                console.log('change', e.target.value)
+            }}
         />
     </div>
 )
