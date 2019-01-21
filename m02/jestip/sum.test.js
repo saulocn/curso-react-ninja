9d7c8ca951df49eb76107f9a57c8d5c3c9697aeb
saulocn <saulocn@gmail.com>
2019-01-21 18:52:39 -0200

    Watch dos tests

--- m02/jestip/sum.test.js
+++ m02/jestip/sum.test.js
@@ -1,7 +1,12 @@
 'use strict'
-import { expect } from 'chai';
+
+import { expect } from 'chai'
 import sum from './sum'
 
 it('sum should be a function', ()=>{
     expect(sum).to.be.a('function')
+})
+
+it('sum(1, 2) should return 3', ()=>{
+    expect(sum(1,2)).to.be.equal(3)
 })
