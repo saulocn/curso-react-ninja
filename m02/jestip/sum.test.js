017c7cc810376d41039b829c850c2ff72922a9a0
saulocn <saulocn@gmail.com>
2019-01-21 18:07:50 -0200

    Jest: Describe, it e expect

--- m02/jestip/sum.test.js
+++ m02/jestip/sum.test.js
@@ -1,5 +1,12 @@
 'use strict'
 
-it('Testando jest', ()=> {
-    
+describe('# ESCOPO', () => {
+
+
+    it('Um é igual a 1', () => {
+        //console.assert(1 === 2, '1 não é igual a 2')
+        expect(1).toBe(2);
+    })
+
+
 })
