2d0cbbaa9e66018e1746ed7e7bf428e329cba363
saulocn <saulocn@gmail.com>
2019-01-30 18:17:38 -0200

    Testes de erros esperados

--- m02/jestip/src/map/map-recursive.js
+++ m02/jestip/src/map/map-recursive.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const map = (arr = [], fn = (item) => item) => {
+    if(!Array.isArray(arr)) throw new TypeError('The first parameter must be an array')
+    if(typeof fn !== 'function') throw new TypeError('The second parameter must be a function')
     return (function mapInternal(arrInternal, counter) {
         const [head, ...tail] = arrInternal
         return (arrInternal.length === 0) ? [] : [
--- m02/jestip/src/map/map.js
+++ m02/jestip/src/map/map.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const map = (arr = [], fn = (item) => item) => {
+    if(!Array.isArray(arr)) throw new TypeError('The first parameter must be an array')
+    if(typeof fn !== 'function') throw new TypeError('The second parameter must be a function')
     let newArr = []
     for (let i = 0; i < arr.length; i++)
         newArr.push(fn(arr[i], i, arr))
