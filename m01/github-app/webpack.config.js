bb1fc4b2e89e9212b41dfc1a3b1187cdc06eb53b
saulocn <saulocn@gmail.com>
2019-02-05 22:28:04 -0200

    Utilizando Html gerado pelo Webpack

--- m01/github-app/webpack.config.js
+++ m01/github-app/webpack.config.js
@@ -2,6 +2,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const validate = require('webpack-validator')
+const HtmlPlugin = require('html-webpack-plugin')
 
 module.exports = validate({
     devtool: 'source-map',
@@ -13,11 +14,15 @@ module.exports = validate({
     ],
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/dist/'
+        filename: '[name]-[hash].js',
+        publicPath: ''
     },
     plugins:[
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlPlugin({
+            title:'Github App',
+            template: path.join(__dirname, 'src', 'html', 'template.html')
+        })
     ],
     module:{
         preLoaders: [{
