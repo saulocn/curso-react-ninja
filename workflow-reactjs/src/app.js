d3763cdcb0af6063bd397c91ce8369c13f0e5616
saulocn <saulocn@gmail.com>
2019-02-05 23:04:21 -0200

    Atualizando o projeto padrão com as novas configurações do webpack

---
+++ workflow-reactjs/src/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React, { Component } from 'react'
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                React!
+            </div>
+        )
+    }
+}
+
+export default App
