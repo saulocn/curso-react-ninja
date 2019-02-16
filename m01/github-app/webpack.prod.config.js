4c495190ba1c07574b866f4e9d4e4d79ad394ca1
saulocn <saulocn@gmail.com>
2019-02-16 10:42:27 -0200

    Trabalhando com assets: Modularizando o CSS

--- m01/github-app/webpack.prod.config.js
+++ m01/github-app/webpack.prod.config.js
@@ -5,6 +5,9 @@ const validate = require('webpack-validator')
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
+const crp = new ExtractTextPlugin('crp.css')
+const styles = new ExtractTextPlugin('[name]-[hash].css')
+
 module.exports = validate({
     entry: path.join(__dirname, 'src', 'index'),
     output: {
@@ -12,6 +15,8 @@ module.exports = validate({
         filename: '[name]-[hash].js',
     },
     plugins: [
+        crp,
+        styles,
         new ExtractTextPlugin('[name]-[hash].css'),
         new webpack.DefinePlugin({
             'process.env': {
@@ -25,6 +30,7 @@ module.exports = validate({
         new webpack.optimize.OccurrenceOrderPlugin(),
         new HtmlPlugin({
             title: 'Github App',
+            inject: false,
             template: path.join(__dirname, 'src', 'html', 'template.html')
         })
     ],
@@ -42,9 +48,14 @@ module.exports = validate({
             loader: 'babel'
         }, {
             test: /\.css$/,
+            exclude: /node_modules|(search|style)\.css/,
+            include: /src/,
+            loader: styles.extract('style', 'css?modules')
+        },{
+            test: /(search|style)\.css$/,
             exclude: /node_modules/,
             include: /src/,
-            loader: ExtractTextPlugin.extract('style', 'css')
+            loader: crp.extract('style', 'css?modules')
         }]
     }
 })
