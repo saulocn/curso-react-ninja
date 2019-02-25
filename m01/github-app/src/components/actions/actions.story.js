ef2574ce3d6ddf312f59b158de014f499337108c
saulocn <saulocn@gmail.com>
2019-02-25 19:45:29 -0300

    Escrevendo stories

--- m01/github-app/src/components/actions/actions.story.js
+++ m01/github-app/src/components/actions/actions.story.js
@@ -1,9 +1,19 @@
 'use strict'
 
-import { storiesOf } from '@kadira/storybook'
+import { storiesOf, action } from '@kadira/storybook'
 import React from 'react'
+import Actions from './index'
 
-storiesOf('Div', module)
-    .add('first story', ()=>(
-        <div>Hello darkness</div>
-    ))
+storiesOf('Actions', module)
+.add('first story', ()=>(
+    <Actions
+    getRepos={action('Get Repos')}
+    getStarred={action('Get Starred')}
+    />
+))
+.add('second story', ()=>(
+    <Actions
+    getRepos={action('Get Repos')}
+    getStarred={action('Get Starred')}
+    />
+))
