d0ee9251914ab76c5b4e63eb329d15e7954f0710
saulocn <saulocn@gmail.com>
2019-03-15 20:04:22 -0300

    Testes de excessões e refatorando o componente

--- m02/applications/github-app/src/utils/pagination/index.js
+++ m02/applications/github-app/src/utils/pagination/index.js
@@ -1,16 +1,18 @@
 'use strict'
 
-const centerRule = ({ total, activePage }) => {
-  if (activePage - 1 <= 0) {
-    return 1
-  }
-  if (activePage === total) {
-    return activePage - 2
-  }
-  return activePage - 1
-}
+const centerRule = ({ total, activePage }) => activePage - 1 <= 0
+  ? 1 : (activePage === total
+    ? activePage - 2 : activePage - 1)
 
-const pagination = ({ total, activePage }) => {
+const isNumber = (value) => typeof value === 'number'
+
+const pagination = ({ total = 1, activePage = 1 } = {}) => {
+  if (!isNumber(total)) {
+    throw new TypeError('total should be a number')
+  }
+  if (!isNumber(activePage)) {
+    throw new TypeError('activePage should be a number')
+  }
   if (total <= 5) {
     return Array.from({ length: total }, (_, i) => i + 1)
   }
