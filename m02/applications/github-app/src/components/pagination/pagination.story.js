5d917e83a80288717cbc9c9415a92dd3284b96bf
saulocn <saulocn@gmail.com>
2019-03-16 09:08:35 -0300

    Criando stories para componente Pagination

--- m02/applications/github-app/src/components/pagination/pagination.story.js
+++ m02/applications/github-app/src/components/pagination/pagination.story.js
@@ -6,5 +6,23 @@ import Pagination from './index'
 
 const stories = storiesOf('<Pagination />', module)
 stories.add('without props', () => (
-  <Pagination total={10} pageLink='http://google.com/page/%page%' />
+  <Pagination />
+))
+
+stories.add('with total and activePage', () => (
+  <Pagination total={10} activePage={5} />
+))
+
+stories.add('with page link', () => (
+  <Pagination total={3} activePage={1} pageLink='http://google.com/page/%page%' />
+))
+
+stories.add('with callback', () => (
+  <Pagination total={15} activePage={8} pageLink='http://google.com/page/%page%'
+    onClick={
+      (page) => {
+        window.alert(page)
+      }
+    }
+  />
 ))
