9a3e006cd7d080843d9aeeb2ac3aed01dbe4517e
saulocn <saulocn@gmail.com>
2019-03-21 22:46:20 -0300

    Iniciando o desenvolvimento do editor markup

---
+++ m02/applications/markdown-editor/src/app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+import React, { Component } from 'react'
+import MarkdownEditor from './markdown-editor'
+
+import 'normalize.css'
+import './css/style.css'
+
+class App extends Component {
+
+  constructor() {
+    super()
+    this.state = { value: '' }
+
+    this.handleChange = e => {
+      this.setState({ value: e.target.value })
+    }
+
+    this.getMarkup = ()  => {
+      return { __html: this.state.value }
+    }
+  }
+
+
+
+  render() {
+    return (
+      <MarkdownEditor
+        value={this.state.value}
+        handleChange={this.handleChange}
+        getMarkup={this.getMarkup}
+      />
+    )
+  }
+}
+export default App
---
+++ m02/applications/markdown-editor/storybook/config.js
@@ -0,0 +1,8 @@
+import { configure } from '@kadira/storybook'
+const req = require.context('../src/components', true, /\.story\.js$/)
+
+function loadStories () {
+  req.keys().forEach(fileName => req(fileName))
+}
+
+configure(loadStories, module)
