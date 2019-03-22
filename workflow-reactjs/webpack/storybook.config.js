6f04431f82b96c4c4b14bd62f49ad6cb45d55aa2
saulocn <saulocn@gmail.com>
2019-03-21 21:45:44 -0300

    Corrigindo loader no webpack do storybook

--- workflow-reactjs/webpack/storybook.config.js
+++ workflow-reactjs/webpack/storybook.config.js
@@ -7,7 +7,7 @@ module.exports = function (config, env) {
   const newConfig = webpackConfig(config, env)
   const preloaders = Object.assign({}, common.standardPreLoader, {
     use: undefined,
-    loader: common.standardPreLoader.use
+    loader: common.standardPreLoader.use.loader
   })
 
   newConfig.module.preLoaders = (newConfig.module.preLoaders || []).concat(preloaders)
